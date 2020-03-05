271def69cb016dcb5335e485f7f5e156248f7b63
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2020-03-05 12:27:23 +0800

    iOS camera working

--- src/modules/Scanner/index.tsx
+++ src/modules/Scanner/index.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { Text, SafeAreaView, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
-const BarcodeScanner = () => (
-  <SafeAreaView>
-    <Text>Scanner Page</Text>
-  </SafeAreaView>
-);
+class BarcodeScanner extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <View style={{ width: '100%', alignItems: 'center' }}>
+          <Text
+            style={{
+              color: 'gray',
+              fontSize: 30,
+            }}
+          >
+            Please Scan
+          </Text>
+        </View>
+        <View
+          style={
+            {
+              width: '100%',
+              height: '75%',
+              backgroundColor: 'green',
+            }
+          }
+        >
+          <RNCamera
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            style={{
+              flex: 1,
+              width: '100%',
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+            }}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
 export default BarcodeScanner;
