73a7130d30d3ff83c966ac3853274622fd8dd90a
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2020-03-06 12:20:54 +0800

    configure firebase for ios to use barcode scanner

--- src/modules/Scanner/index.tsx
+++ src/modules/Scanner/index.tsx
@@ -1,43 +1,86 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import { Text, SafeAreaView, View } from 'react-native';
+import {
+  View, StyleSheet, Text, Linking,
+} from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  scanner: {
+    flex: 1,
+    width: '100%',
+  },
+});
 class BarcodeScanner extends Component {
+  state = {
+    barcodes: [],
+  }
+
+  barcodeRecognized = ({ barcodes }) => {
+    this.setState({ barcodes });
+  }
+
+  openURL = (url) => {
+    Linking.openURL(url).catch((err) => console.error('An error occurred', err));
+  }
+
+  renderBarcode = ({ bounds, data }) => (
+    <React.Fragment key={data + bounds.origin.x}>
+      <View
+        style={{
+          borderWidth: 2,
+          borderRadius: 10,
+          position: 'absolute',
+          borderColor: '#F00',
+          justifyContent: 'center',
+          backgroundColor: 'rgba(255, 255, 255, 0.9)',
+          height: bounds.size.height,
+          width: bounds.size.width + 50,
+          left: bounds.origin.x,
+          top: bounds.origin.y,
+        }}
+      >
+        <Text
+          style={{
+            color: '#F00',
+            flex: 1,
+            position: 'absolute',
+            textAlign: 'center',
+            backgroundColor: 'transparent',
+          }}
+          onPress={() => this.openURL('https://apps.apple.com/sg/app/mocha-tn5250/id521454323?mt=12')}
+        >
+          open app
+        </Text>
+      </View>
+    </React.Fragment>
+  );
+
+  renderBarcodes = () => {
+    const { barcodes } = this.state;
+    return (
+      <View>{barcodes.map(this.renderBarcode)}</View>
+    );
+  }
+
   render() {
     return (
-      <SafeAreaView>
-        <View style={{ width: '100%', alignItems: 'center' }}>
-          <Text
-            style={{
-              color: 'gray',
-              fontSize: 30,
-            }}
-          >
-            Please Scan
-          </Text>
-        </View>
-        <View
-          style={
-            {
-              width: '100%',
-              height: '75%',
-              backgroundColor: 'green',
-            }
-          }
+      <View style={styles.container}>
+        <RNCamera
+          ref={(ref) => {
+            this.camera = ref;
+          }}
+          style={styles.scanner}
+          onGoogleVisionBarcodesDetected={this.barcodeRecognized}
         >
-          <RNCamera
-            type={RNCamera.Constants.Type.back}
-            flashMode={RNCamera.Constants.FlashMode.on}
-            style={{
-              flex: 1,
-              width: '100%',
-              justifyContent: 'flex-end',
-              alignItems: 'center',
-            }}
-          />
-        </View>
-      </SafeAreaView>
+          {this.renderBarcodes()}
+        </RNCamera>
+      </View>
     );
   }
 }
